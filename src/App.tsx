a183a5194060b4e5eba503f8c0e38767f6a12965
bananabread08 <rmkfermin@gmail.com>
2022-11-14 08:40:22 +0800

    create components folder, create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World</h1>
+    <div className=" h-screen bg-slate-400">
+      <Navbar />
+      <h1 className=" p-2 bg-red-200 text-neutral-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        labore sapiente mollitia minus explicabo ipsum ratione, quae eos, quis
+        aliquid ad? Est, tenetur cupiditate. Deleniti inventore recusandae eum
+        dolores debitis nemo, expedita sit ut ea similique? Dolore, at dolorem!
+        Repellendus laborum aliquid asperiores animi ipsum debitis ut, molestiae
+        nesciunt natus, earum omnis molestias quia. Illum dicta rem laudantium.
+        Officia corrupti fugit atque odio maxime esse sequi quas debitis et
+        quibusdam inventore, fuga aperiam molestias suscipit culpa aspernatur.
+        Tempore sint, quos nam corrupti, nemo pariatur officiis, laudantium quae
+        doloremque ab voluptatum illum voluptates minima sequi commodi ullam
+        accusantium facilis tenetur consectetur.
+      </h1>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = (): JSX.Element => {
+  return <nav className=" w-full p-2 bg-slate-500">Navbar</nav>;
+};
+
+export default Navbar;

1e9b8eda164555032a28eaaa867957eb803da593
bananabread08 <rmkfermin@gmail.com>
2022-11-17 09:10:23 +0800

    use react-router to create basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World</div>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )

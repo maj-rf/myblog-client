d5aa342a84789dcdd4e81b3c8596ecabc0ab747d
bananabread08 <rmkfermin@gmail.com>
2022-12-21 09:13:41 +0800

    use getPosts to fetch and show all posts from server

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,31 @@
-export const Home = () => {
-  return <div>This is the Home Page</div>
+import { useState, useEffect } from 'react'
+import { getPosts } from '../api/postAPI'
+import { TPost } from '../types/post'
+
+export const Home = (): JSX.Element => {
+  const [posts, setPosts] = useState<TPost[]>([])
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const posts = await getPosts()
+      setPosts(posts)
+    }
+    fetchPosts()
+  }, [])
+
+  return (
+    <div>
+      <h1>This is the Home Page</h1>
+      <ul>
+        {posts?.map((post) => {
+          return (
+            <li key={post._id}>
+              <h1>{post.title}</h1>
+              <p className=" ml-4">{post.content}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }

429946769db864a3e3fa90268b4d395e4ab1895b
bananabread08 <rmkfermin@gmail.com>
2022-12-21 10:09:49 +0800

    update Login and Signup styles

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,55 @@
 export const Signup = () => {
-  return <div>Signup</div>
+  return (
+    <section>
+      <div className=" flex flex-wrap justify-center mt-20">
+        <div className="w-full max-w-sm">
+          <form className=" shadow-md bg-white rounded px-8 py-6 flex flex-col gap-3">
+            <h1 className="text-2xl">Create an account</h1>
+            <div>
+              <label htmlFor="username">Username</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                type="text"
+                name="username"
+                placeholder="your_username"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                type="email"
+                name="email"
+                placeholder="your_email@gmail.com"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                type="password"
+                name="password"
+                placeholder="*******"
+              />
+            </div>
+            <div>
+              <label htmlFor="confirm-pass">Confirm Password</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                type="password"
+                name="confirm-pass"
+                placeholder="*******"
+              />
+            </div>
+            <button
+              className=" bg-orange-400 py-2 rounded text-white text-xl hover:bg-orange-600"
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
 }

2bc9a28f9b236a1acc0f5139f8b69b26cc701004
bananabread08 <rmkfermin@gmail.com>
2023-04-27 09:46:35 +0800

    initial commit
    install packages

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className=" bg-slate-600">Test</div>
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
-import { Root } from './components/Root'
-import { Home, loader as homeLoader } from './pages/Home'
-import Login from './pages/Login'
-import { Signup, action as signupAction } from './pages/Signup'
-import { AuthProvider } from './context/auth'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        loader: homeLoader,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'signup',
-        element: <Signup />,
-        action: signupAction,
-      },
-    ],
-  },
-])
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <App />
   </React.StrictMode>,
-)
+);

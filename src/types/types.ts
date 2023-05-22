b33a9e864d2ef3048987ff4467a0033744a0c0a5
bananabread08 <rmkfermin@gmail.com>
2023-05-22 10:19:01 +0800

    create initial types

---
+++ src/types/types.ts
@@ -0,0 +1,51 @@
+export interface IComment {
+  content: string;
+  user: string; // userId
+  blog: string; // blogId
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IBlog {
+  user: string;
+  title: string;
+  content: string;
+  published: boolean;
+  tags: string[];
+  comments: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface INewUserCredentials {
+  username: string;
+  email: string;
+  password: string;
+  confirm_pass: string;
+}
+
+export interface IUserCredentials {
+  email: string;
+  password: string;
+}
+
+export interface ILoginResponse {
+  accessToken: string;
+}
+
+export interface IGenericResponse {
+  message: string;
+}
+
+export type PublicUser = Pick<IUser, 'id' | 'username' | 'email'>;

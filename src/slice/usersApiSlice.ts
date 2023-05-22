85561595e36fde8844569aea3d8cebb907e5edfd
bananabread08 <rmkfermin@gmail.com>
2023-05-22 21:31:20 +0800

    add register mutation

--- src/slice/usersApiSlice.ts
+++ src/slice/usersApiSlice.ts
@@ -15,7 +15,15 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: 'POST',
       }),
     }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: '/auth/register',
+        method: 'POST',
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
+  usersApiSlice;

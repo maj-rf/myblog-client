f563f836b3a152b0b47a2c5fb530322a6384e8fc
bananabread08 <rmkfermin@gmail.com>
2023-05-23 10:48:20 +0800

    create apis slice for blogs

---
+++ src/slice/blogsApiSlice.ts
@@ -0,0 +1,16 @@
+import { IBlog } from '../types/types';
+import { apiSlice } from './apiSlice';
+
+export const blogsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllBlogs: builder.query<IBlog[], void>({
+      query: () => 'blogs',
+    }),
+    getBlog: builder.query<IBlog, string>({
+      query: (id) => `blogs/${id}`,
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const { useGetAllBlogsQuery, useGetBlogQuery } = blogsApiSlice;

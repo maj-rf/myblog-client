174695915f158a2a650898376559fe9d18d381b4
bananabread08 <rmkfermin@gmail.com>
2023-05-25 10:54:00 +0800

    update blogApiSlice with new endpoints

--- src/slice/blogsApiSlice.ts
+++ src/slice/blogsApiSlice.ts
@@ -4,14 +4,18 @@ import { apiSlice } from './apiSlice';
 export const blogsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getAllBlogs: builder.query<IBlog[], void>({
-      query: () => 'blogs',
+      query: () => 'blogs/all',
       providesTags: ['Blog'],
     }),
     getBlog: builder.query<IBlog, string>({
-      query: (id) => `blogs/${id}`,
+      query: (id) => `blogs/blog/${id}`,
+    }),
+    getProfileBlogs: builder.query<IBlog[], void>({
+      query: () => 'blogs/profile',
     }),
   }),
   overrideExisting: false,
 });
 
-export const { useGetAllBlogsQuery, useGetBlogQuery } = blogsApiSlice;
+export const { useGetAllBlogsQuery, useGetBlogQuery, useGetProfileBlogsQuery } =
+  blogsApiSlice;

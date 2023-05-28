b0cd2842fda4ffc4b46c2bfb38e11ab03abbc782
bananabread08 <rmkfermin@gmail.com>
2023-05-28 11:34:37 +0800

    update cache tags in blog and comment

--- src/slice/blogsApiSlice.ts
+++ src/slice/blogsApiSlice.ts
@@ -5,7 +5,13 @@ export const blogsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getAllBlogs: builder.query<IBlog[], void>({
       query: () => 'blogs/all',
-      providesTags: ['Blog'],
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Blog' as const, id })),
+              { type: 'Blog', id: 'LIST' },
+            ]
+          : [{ type: 'Blog', id: 'LIST' }],
     }),
     getBlog: builder.query<IBlog, string>({
       query: (id) => `blogs/blog/${id}`,
--- src/slice/commentApiSli.ts
+++ src/slice/commentApiSli.ts
@@ -5,8 +5,24 @@ export const commentApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getBlogComments: builder.query<IComment[], string>({
       query: (id) => `comments/${id}`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Comment' as const, id })),
+              { type: 'Comment', id: 'LIST' },
+            ]
+          : [{ type: 'Comment', id: 'LIST' }],
+    }),
+    addComment: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `comments/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Comment'],
     }),
   }),
 });
 
-export const { useGetBlogCommentsQuery } = commentApiSlice;
+export const { useGetBlogCommentsQuery, useAddCommentMutation } =
+  commentApiSlice;
